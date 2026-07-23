a676a799e0fc2798812452bc7efbae4bf83315c4
Glitchtrap991 <vaparajith.2024@gmail.com>
2026-07-23 22:07:43 +0530

    fix: add tailwindcss and vite plugin to package.json

--- MyFirstReactApp/vite.config.js
+++ MyFirstReactApp/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  // ADD THIS LINE: It must be exactly your GitHub repo name surrounded by slashes
+  base: "/portfolio-website/", 
 })
