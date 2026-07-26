2f007ce03fed7c3bb88cf1157e4d432ea557a88e
Aparajith.V <vaparajith.2024@gmail.com>
2026-07-26 17:46:35 +0530

    Remove duplicate HashRouter import

--- MyFirstReactApp/src/main.jsx
+++ MyFirstReactApp/src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {ReactDOM} from "react-dom/client";
 //import {HashRouter} from "react-router-dom";
-
-import {HashRouter} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import {HashRouter} from "react-router-dom";
